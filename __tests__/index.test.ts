3c414797d7755ff1cff05707a772cd627283204e
_Kerman <KermanX@qq.com>
2022-05-22 16:31:49 +0800

    release v1.0.0

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,21 +1,190 @@
-import { RangeSet } from "../dist/index.js";
+import { parseRanges, RangeSet, stringifyRanges } from "../dist/index.js";
 
 describe("Test set of range", () => {
+  it("should parse ranges correctly", () => {
+    expect(
+      parseRanges("( - Infinity,1](23, 45)[ 67,89] [100  ,infinity)")
+    ).toEqual(data_range_1);
+    expect(
+      parseRanges("(-Infinity,1](23,45)[67,89] [10 0  ,+infinity)")
+    ).toEqual(data_range_1);
+    expect(() => parseRanges("[abcd,123)")).toThrowError();
+    expect(() => parseRanges("[12?34)")).toThrowError();
+    expect(() => parseRanges("[12,34)(56")).toThrowError();
+    expect(() => parseRanges("[12,34)(56,78))")).toThrowError();
+  });
+  it("should stringify ranges correctly", () => {
+    expect(stringifyRanges(data_range_1)).toBe(
+      "(-Infinity,1](23,45)[67,89][100,Infinity)"
+    );
+  });
+  it("should construct correctly", () => {
+    expect(new RangeSet("(23, 45)[67,89 ]").ranges).toEqual(
+      parseRanges("(23, 45)[67,89 ]")
+    );
+    expect(new RangeSet(data_range_1).ranges).toBe(data_range_1);
+    expect(() => new RangeSet({} as any)).toThrowError();
+  });
   it("should operate union correctly.", () => {
-    let x = new RangeSet([10, 30, 50, 100]),
-      y = new RangeSet([20, 40, 110, 120]);
-    expect(x.union(x, y).ranges).toEqual([10, 40, 50, 100, 110, 120]);
+    let x = new RangeSet("[10,30][50,100]"),
+      y = new RangeSet("[20,40][110,120]");
+    expect(x.union(y).ranges).toEqual(parseRanges("[10,40][50,100][110,120]"));
+
+    x = new RangeSet("(20,30][50,100]");
+    y = new RangeSet("[20,40][110,120]");
+    expect(x.union(y).ranges).toEqual(parseRanges("[20,40][50,100][110,120]"));
+
+    x = new RangeSet("(-Infinity,30][50,100]");
+    y = new RangeSet("[20,40][110,120]");
+    expect(x.union(y).ranges).toEqual(
+      parseRanges("(-Infinity,40][50,100][110,120]")
+    );
+
+    x = new RangeSet("(20,30][50,infinity)");
+    y = new RangeSet("[20,40][110,120]");
+    expect(x.union(y).ranges).toEqual(parseRanges("[20,40][50,infinity)"));
+
+    x = new RangeSet("(20,30][50,60][70,80]");
+    y = new RangeSet("[20,40][110,120]");
+    expect(x.union(y).ranges).toEqual(
+      parseRanges("[20,40][50,60][70,80][110,120]")
+    );
+
+    x = new RangeSet("[20,20]");
+    y = new RangeSet("[30,30]");
+    expect(x.union(y).ranges).toEqual(parseRanges("[20,20][30,30]"));
   });
   it("should operate intersection correctly.", () => {
-    let x = new RangeSet([10, 30, 50, 100]),
-      y = new RangeSet([20, 40, 110, 120]);
-    expect(x.intersection(x, y).ranges).toEqual([20, 30]);
+    let x = new RangeSet("[10,30][50,100]"),
+      y = new RangeSet("[20,40][110,120]");
+    expect(x.intersection(y).ranges).toEqual(parseRanges("[20,30]"));
+    
+    x = new RangeSet("[10,20]");
+    y = new RangeSet("[30,40]");
+    expect(x.intersection(y).ranges).toEqual(parseRanges(""));
+  });
+  it("should do customized operation correctly.", () => {
+    const x = new RangeSet("[10,20]"),
+      y = new RangeSet("[30,40]");
+    expect(
+      x.operate(
+        [
+          [false, false],
+          [false, true],
+        ],
+        y
+      ).ranges
+    ).toEqual(parseRanges("(-infinity,10)(20,30)(40,infinity)"));
+  });
+  it("should judge subsets correctly", () => {
+    expect(
+      new RangeSet("[30,40]").isSubsetOf(new RangeSet("[20,40][110,120]"))
+    ).toBeTruthy();
+    expect(
+      new RangeSet("[30,40]").isSubsetOf(new RangeSet("[20,40)[110,120]"))
+    ).toBeFalsy();
+    expect(
+      new RangeSet("(-Infinity,40]").isSubsetOf(
+        new RangeSet("(-Infinity,50][110,120]")
+      )
+    ).toBeTruthy();
+    expect(
+      new RangeSet("(-Infinity,40]").isSubsetOf(new RangeSet("[0,50][110,120]"))
+    ).toBeFalsy();
+    expect(
+      new RangeSet("[30,40]").isSubsetOf(new RangeSet("[30,40]"))
+    ).toBeTruthy();
+    expect(
+      new RangeSet("[30,40]").isProperSubsetOf(new RangeSet("[30,40]"))
+    ).toBeFalsy();
+  });
+  it("should calc num of ranges correctly", () => {
+    expect(new RangeSet("[30,40][50,60]").numOfRanges).toBe(2);
   });
   it("should prettier ranges correctly", () => {
-    let x = new RangeSet([10, 30, 50, 100]);
+    let x = new RangeSet("[10,30][50,100]");
     expect(x.prettier).toEqual([
-      [10, 30],
-      [50, 100],
+      {
+        left: {
+          equal: true,
+          value: 10,
+        },
+        right: {
+          equal: true,
+          value: 30,
+        },
+      },
+      {
+        left: {
+          equal: true,
+          value: 50,
+        },
+        right: {
+          equal: true,
+          value: 100,
+        },
+      },
     ]);
   });
+  it("should judge whether it is empty correctly", () => {
+    expect(new RangeSet("[10,30][50,100]").isEmpty()).toBeFalsy();
+    expect(new RangeSet([]).isEmpty()).toBeTruthy();
+  });
+  it("should judge whether it is valid correctly", () => {
+    const x = new RangeSet("[10,30][50,100]");
+    x.ranges.push({ value: 2000, equal: true });
+    expect(x.isValid()).toBeFalsy();
+    expect(new RangeSet("[10,30][50,10]").isValid()).toBeFalsy();
+    expect(new RangeSet("[10,30][50,100]").isValid()).toBeTruthy();
+    expect(new RangeSet("(-infinity,30][50,100]").isValid()).toBeTruthy();
+    expect(new RangeSet("[-infinity,30][50,100]").isValid()).toBeFalsy();
+    expect(new RangeSet("[10,30][50,infinity)").isValid()).toBeTruthy();
+    expect(new RangeSet("[10,30][50,infinity]").isValid()).toBeFalsy();
+  });
+  it("should judge whether the two are the same correctly", () => {
+    expect(
+      new RangeSet("[10,30][50,100]").isSame(new RangeSet("[10,30][50,100]"))
+    ).toBeTruthy();
+    expect(
+      new RangeSet("[10,30][50,100]").isSame(new RangeSet("[10,30][50,200]"))
+    ).toBeFalsy();
+    const x = new RangeSet("[10,30][50,100]");
+    x.ranges.push({ value: 2000, equal: true });
+    expect(new RangeSet("[10,30][50,100]").isSame(x)).toBeFalsy();
+  });
 });
+
+const data_range_1 = [
+  {
+    value: -Infinity,
+    equal: false,
+  },
+  {
+    value: 1,
+    equal: true,
+  },
+  {
+    value: 23,
+    equal: false,
+  },
+  {
+    value: 45,
+    equal: false,
+  },
+  {
+    value: 67,
+    equal: true,
+  },
+  {
+    value: 89,
+    equal: true,
+  },
+  {
+    value: 100,
+    equal: true,
+  },
+  {
+    value: Infinity,
+    equal: false,
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+export interface Endpoint {
+  value: number;
+  equal: boolean;
+}
+
+export type Ranges = Endpoint[];
+
 export class RangeSet {
-  constructor(ranges: number[] = []) {
-    this.ranges = ranges;
+  constructor(ranges: Ranges | string = []) {
+    if (Array.isArray(ranges)) this.ranges = ranges;
+    else if (typeof ranges === "string") this.ranges = parseRanges(ranges);
+    else throw new TypeError("Unknown initial ranges");
   }
-  ranges: number[];
+  ranges: Ranges;
   operate(table: OperateTruthTable, ...rs: RangeSet[]): RangeSet {
     return operate(
       table,
@@ -16,10 +25,49 @@ export class RangeSet {
   intersection(...rs: RangeSet[]): RangeSet {
     return this.operate(truthTable_intersection, ...rs);
   }
-  get prettier() {
-    let prettier: [number, number][] = [];
+
+  isSubsetOf(rs: RangeSet): boolean {
+    return this.intersection(rs).isSame(this);
+  }
+  isProperSubsetOf(rs: RangeSet): boolean {
+    return !this.isSame(rs) && this.isSubsetOf(rs);
+  }
+  isSame(rs: RangeSet): boolean {
+    if (this.ranges.length !== rs.ranges.length) return false;
+    for (let i = 0; i < this.ranges.length; i++) {
+      if (
+        this.ranges[i].value !== rs.ranges[i].value ||
+        this.ranges[i].equal !== rs.ranges[i].equal
+      )
+        return false;
+    }
+    return true;
+  }
+  isEmpty(): boolean {
+    return this.ranges.length === 0;
+  }
+  isValid(): boolean {
+    if (this.ranges.length % 2 !== 0) return false;
+    for (let i = 1; i < this.ranges.length; i++) {
+      if (this.ranges[i - 1].value >= this.ranges[i].value) return false;
+    }
+    if (this.ranges[0].value === -Infinity && this.ranges[0].equal)
+      return false;
+    if (
+      this.ranges[this.ranges.length - 1].value === Infinity &&
+      this.ranges[this.ranges.length - 1].equal
+    )
+      return false;
+    return true;
+  }
+
+  get numOfRanges(): number {
+    return this.ranges.length / 2;
+  }
+  get prettier(): { left: Endpoint; right: Endpoint }[] {
+    let prettier: { left: Endpoint; right: Endpoint }[] = [];
     for (let i = 0; i < this.ranges.length; i += 2)
-      prettier.push([this.ranges[i], this.ranges[i + 1]]);
+      prettier.push({ left: this.ranges[i], right: this.ranges[i + 1] });
     return prettier;
   }
 }
@@ -29,6 +77,14 @@ export type OperateTruthTable = [
   /*L:true */ [boolean, boolean],
   /*L:flase*/ [boolean, boolean]
 ];
+
+export function calcTruthTable(
+  table: OperateTruthTable,
+  l: boolean,
+  r: boolean
+) {
+  return table[l ? 0 : 1][r ? 0 : 1];
+}
 
 export const truthTable_union: OperateTruthTable = [
   [true, true],
@@ -46,30 +102,213 @@ export function operate(
 ): RangeSet {
   let r1 = rs1.ranges,
     r2 = rs2.ranges,
-    r = new RangeSet();
-  let p1 = -1,
-    p2 = -1;
-  let s1 = false,
-    s2 = false,
-    s = false;
-  while (p1 + 1 <= r1.length && p2 + 1 <= r2.length) {
-    let crt = Math.max(
-      p1 === -1 ? -Infinity : r1[p1],
-      p2 === -1 ? -Infinity : r2[p2]
-    );
-    let b1 = s1,
-      b2 = s2;
-    if (r1[p1 + 1] < r2[p2 + 1] || p2 + 1 === r2.length) {
-      p1++;
-      s1 = !s1;
-    } else {
-      p2++;
-      s2 = !s2;
+    result: Ranges = [];
+  let arr: {
+    value: number;
+    equal: boolean;
+    whose: 0x1 | 0x2 | 0x3;
+    type: "start" | "end";
+  }[] = [];
+  let arr2: {
+    value: number;
+    equal: boolean;
+    t: boolean;
+  }[] = [];
+  {
+    let i1 = 0,
+      i2 = 0;
+    let t1 = false,
+      t2 = false;
+    while (i1 < r1.length && i2 < r2.length) {
+      const v1 = r1[i1].value,
+        v2 = r2[i2].value,
+        e1 = r1[i1].equal,
+        e2 = r2[i2].equal;
+      let crtValue: number;
+      let crtEqual: boolean;
+      let crtWhose: 0x1 | 0x2 | 0x3;
+      let crtType: "start" | "end";
+
+      const x1 = (t1 ? -1 : 1) * (e1 ? 1 : 2);
+      const x2 = (t2 ? -1 : 1) * (e2 ? 1 : 2);
+      if (v1 > v2 || (v1 === v2 && x1 > x2)) {
+        crtValue = v2;
+        crtEqual = e2;
+        crtWhose = 0x2;
+        crtType = t2 ? "end" : "start";
+        i2++;
+        t2 = !t2;
+      } else if (v2 > v1 || (v1 === v2 && x1 > x2)) {
+        crtValue = v1;
+        crtEqual = e1;
+        crtWhose = 0x1;
+        crtType = t1 ? "end" : "start";
+        i1++;
+        t1 = !t1;
+      } else {
+        crtValue = v1 /*===v2*/;
+        crtEqual = e1 /*===e2*/;
+        crtWhose = 0x3;
+        crtType = t1 /*===t2*/ ? "end" : "start";
+        i1++;
+        i2++;
+        t1 = !t1;
+        t2 = !t2;
+      }
+      arr.push({
+        value: crtValue,
+        equal: crtEqual,
+        whose: crtWhose,
+        type: crtType,
+      });
     }
-    if (table[b1 ? 0 : 1][b2 ? 0 : 1] !== s) {
-      r.ranges.push(crt);
-      s = !s;
+    if (i1 < r1.length) {
+      for (let j = i1; j < r1.length; j++) {
+        const crt = r1[j];
+        arr.push({
+          value: crt.value,
+          equal: crt.equal,
+          whose: 0x1,
+          type: t1 ? "end" : "start",
+        });
+        t1 = !t1;
+      }
+    } else if (i2 < r2.length) {
+      for (let j = i2; j < r2.length; j++) {
+        const crt = r2[j];
+        arr.push({
+          value: crt.value,
+          equal: crt.equal,
+          whose: 0x2,
+          type: t2 ? "end" : "start",
+        });
+        t2 = !t2;
+      }
     }
   }
-  return r;
+  {
+    if (arr[0].value !== -Infinity) {
+      arr2.push({
+        value: -Infinity,
+        equal: false,
+        t: calcTruthTable(table, false, false),
+      });
+    }
+    let t1 = false,
+      t2 = false;
+    for (const a of arr) {
+      let eq = a.equal;
+      if (a.type === "start") {
+        if (a.whose & 0x1) t1 = true;
+        if (a.whose & 0x2) t2 = true;
+      } else {
+        eq = !eq;
+        if (a.whose & 0x1) t1 = false;
+        if (a.whose & 0x2) t2 = false;
+      }
+      arr2.push({
+        value: a.value,
+        equal: eq,
+        t: calcTruthTable(table, t1, t2),
+      });
+    }
+  }
+  {
+    let t = false;
+    for (const a of arr2) {
+      if (t !== a.t) {
+        t = a.t;
+        result.push({
+          value: a.value,
+          equal: t ? a.equal : !a.equal,
+        });
+      }
+    }
+    if (t) {
+      result.push({
+        value: Infinity,
+        equal: false,
+      });
+    }
+  }
+  return new RangeSet(result);
+}
+
+export function parseRanges(str: string): Ranges {
+  const errMsg = "parseError: Invalid set of ranges.";
+  let ranges: Ranges = [];
+  str = str.replace(/\s/g, "");
+  let v = "";
+  let t = 0;
+  let e1 = false;
+  function parseV(v: string): number {
+    v = v.toLowerCase();
+    if (v === "infinity" || v === "+infinity") {
+      return Infinity;
+    }
+    if (v === "-infinity") {
+      return -Infinity;
+    }
+    return parseInt(v);
+  }
+  for (const c of str) {
+    if (c === "(") {
+      if (t++ !== 0) throw new Error(errMsg);
+      e1 = false;
+    } else if (c === "[") {
+      if (t++ !== 0) throw new Error(errMsg);
+      e1 = true;
+    } else if (c === ",") {
+      if (t++ !== 1) throw new Error(errMsg);
+      let value = parseV(v);
+      if (isNaN(value)) throw new Error(errMsg + `("${v}" is not a number)`);
+      ranges.push({
+        value,
+        equal: e1,
+      });
+      v = "";
+    } else if (c === ")") {
+      if (t !== 2) throw new Error(errMsg);
+      t = 0;
+      let value = parseV(v);
+      if (isNaN(value)) throw new Error(errMsg);
+      ranges.push({
+        value,
+        equal: false,
+      });
+      v = "";
+    } else if (c === "]") {
+      if (t !== 2) throw new Error(errMsg);
+      t = 0;
+      let value = parseV(v);
+      if (isNaN(value)) throw new Error(errMsg);
+      ranges.push({
+        value,
+        equal: true,
+      });
+      v = "";
+    } else {
+      if (t === 0) throw new Error(errMsg);
+      v += c;
+    }
+  }
+  if (t !== 0) throw new Error(errMsg);
+  return ranges;
+}
+
+export function stringifyRanges(ranges: Ranges) {
+  let str = "";
+  let left = true;
+  for (const r of ranges) {
+    if (left) {
+      str += r.equal ? "[" : "(";
+      str += r.value.toString();
+      str += ",";
+    } else {
+      str += r.value.toString();
+      str += r.equal ? "]" : ")";
+    }
+    left = !left;
+  }
+  return str;
 }
